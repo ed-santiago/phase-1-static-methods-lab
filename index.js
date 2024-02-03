435c1ed75d76554f6c2146bf3c40011882052047
Edmond <edmond.santiago7@gmail.com>
2024-02-04 10:29:24 +1100

    complete lab

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
 class Formatter {
-  //add static methods here
+  static capitalize(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+
+  static sanitize(string) {
+    return string.replace(/[^A-Za-z0-9-' ]+/g, "");
+  }
+
+  static titleize(string) {
+    const excludedWords = ["the", "a", "an", "but", "of", "and", "for", "at", "by", "from"];
+    return string.split(' ').map((word, index) => {
+      if (excludedWords.find(exclude => exclude === word)) {
+        if (index === 0) {
+          return word.charAt(0).toUpperCase() + word.slice(1);
+        } else {
+          return word;
+        }
+      }
+      return word.charAt(0).toUpperCase() + word.slice(1);
+    }).join(' ');
+  }
 }
+
+Formatter.titleize("edmond is the best in the world")
